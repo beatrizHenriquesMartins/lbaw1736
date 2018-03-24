12e6f70fd38c1f82e9d3fdf0cb5ed854022784f0
francis-andrade <up201503481@fe.up.pt>
2018-03-24 19:49:27 +0000

    update triggers

--- src/sql/triggers.sql
+++ src/sql/triggers.sql
@@ -1,4 +1,12 @@
-/*Trigger that prevents an admin from being banned*/
+﻿/*
+Trigger that prevents an admin from being banned
+Test: INSERT INTO ban VALUES(1, 2, DEFAULT);
+*/
+DROP FUNCTION IF EXISTS ban_admin() CASCADE;
+DROP TRIGGER IF EXISTS ban_admin ON ban CASCADE;
+DROP FUNCTION IF EXISTS purchaseProduct_cost() CASCADE;
+DROP TRIGGER IF EXISTS purchaseProduct_cost ON purchaseProduct CASCADE;
+
 CREATE FUNCTION ban_admin() RETURNS TRIGGER AS
 $BODY$
 BEGIN
@@ -11,17 +19,41 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER ban_admin
-	BEFORE INSERT ON ban
+	BEFORE INSERT OR UPDATE ON ban
 	FOR EACH ROW
 		EXECUTE PROCEDURE ban_admin();
 
+
 /*
-client -> cart NN
-corrigir isto sempre
-eliminar o cart e as wishlist
-eliminar chat
-dependencias funcionais no productReview
-justificação da forma Normal do esquema
-link para github
-criar domínios
-*/		
+Trigger that actualizes the of a product in a purchase according to its actual value
+TEST: INSERT INTO purchase VALUES (2, 11, 1, DEFAULT, 'verificacao', 300, 'cartao credito', '5400 4102 4021 7362', 'Pedro Gonçalves', '2018-07-23', 235123482); 
+INSERT INTO purchaseProduct VALUES (2, 4, 1, 1)
+*/
+CREATE FUNCTION purchaseProduct_cost() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+	UPDATE purchaseProduct
+	SET cost = (SELECT price FROM PRODUCT WHERE id=NEW.id_product);
+	RETURN NEW;
+END	
+$BODY$
+LANGUAGE plpgsql;	
+
+CREATE TRIGGER purchaseProduct_cost
+	AFTER INSERT ON purchaseProduct
+	FOR EACH ROW
+		EXECUTE PROCEDURE purchaseProduct_cost();
+		
+/*CREATE FUNCTION purchase_cost() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+	UPDATE purchase
+	SET Cost = SELECT 
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER purchase_cost
+	AFTER INSERT ON purchase
+	FOR EACH ROW
+		EXECUTE PROCEDURE purchase_cost();*/
