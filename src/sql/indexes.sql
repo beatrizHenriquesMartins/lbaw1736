e5d3c3f5de7a1cf6ad2525b3cc24888b5ef637d3
Luis Saraiva <up201404302@fe.up.pt>
2018-04-01 20:39:06 +0100

    Add comments to Indexes

--- src/sql/indexes.sql
+++ src/sql/indexes.sql
@@ -1,15 +1,15 @@
-CREATE INDEX idx_users ON users USING hash (id);
+CREATE INDEX idx_users ON users USING hash (id); /*cardinalidade alta-nao se faz cluster*/
 
-CREATE INDEX idx_message ON message USING btree (id_client);
+CREATE INDEX idx_message ON message USING btree (id_client); /*cardinalidade media-bom candidato para cluster*/
 
-CREATE INDEX idx_product ON product USING btree (name);
+CREATE INDEX idx_product ON product USING btree (name); /*cardinalidade media-bom candidato para cluster*/
 
-CREATE INDEX idx_productwishlist ON productwishlist USING hash (id_client);
+CREATE INDEX idx_productwishlist ON productwishlist USING hash (id_client); /*cardinalidade media-bom candidato para cluster*/
 
-CREATE INDEX idx_cartproduct ON cartproduct USING hash (id_client);
+CREATE INDEX idx_cartproduct ON cartproduct USING hash (id_client); /*cardinalidade media-bom candidato para cluster*/
 
-CREATE INDEX idx_purchase ON purchase USING btree (id_client);
+CREATE INDEX idx_purchase ON purchase USING btree (id_client); /*cardinalidade media-bom candidato para cluster*/
 
-CREATE INDEX idx_purchaseproduct ON purchaseproduct USING hash (id_purchase);
+CREATE INDEX idx_purchaseproduct ON purchaseproduct USING hash (id_purchase); /*cardinalidade alta-nao se faz cluster*/
 
-CREATE INDEX idx_productreview ON productreview USING hash (id_product);
+CREATE INDEX idx_productreview ON productreview USING hash (id_product); /*cardinalidade media-bom candidato para cluster*/
