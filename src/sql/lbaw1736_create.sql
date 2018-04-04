27723b8bafaaef5ff29d5f015034f1ad3b6791d0
Luis Saraiva <up201404302@fe.up.pt>
2018-04-04 09:28:43 +0100

    Added Queries to create.sql

--- src/sql/lbaw1736_create.sql
+++ src/sql/lbaw1736_create.sql
@@ -25,6 +25,35 @@ DROP TRIGGER IF EXISTS ban_admin ON ban CASCADE;
 DROP FUNCTION IF EXISTS purchase_cost() CASCADE;
 DROP TRIGGER IF EXISTS purchase_cost ON purchase CASCADE;
 
+DROP VIEW IF EXISTS "ListProducts";
+DROP VIEW IF EXISTS "ViewProduct";
+DROP VIEW IF EXISTS "ListProductsByCategory";
+DROP VIEW IF EXISTS "ReviewsByProductId";
+DROP VIEW IF EXISTS "ReviewsByPurchaseId";
+DROP VIEW IF EXISTS "ListBrands";
+DROP VIEW IF EXISTS "BrandProducts";
+DROP VIEW IF EXISTS "ListUsers";
+DROP VIEW IF EXISTS "Profile";
+DROP VIEW IF EXISTS "Supports";
+DROP VIEW IF EXISTS "BrandManagers";
+DROP VIEW IF EXISTS "BrandManagersByBrand";
+DROP VIEW IF EXISTS "WishlistByClientId";
+DROP VIEW IF EXISTS "PurchasesByClientId";
+DROP VIEW IF EXISTS "ProductsInPurchase";
+DROP VIEW IF EXISTS "ProductsInCart";
+DROP VIEW IF EXISTS "ClientMessages";
+
+DROP INDEX IF EXISTS idx_users;
+DROP INDEX IF EXISTS idx_message;
+DROP INDEX IF EXISTS idx_product;
+DROP INDEX IF EXISTS idx_productwishlist;
+DROP INDEX IF EXISTS idx_cartproduct;
+DROP INDEX IF EXISTS idx_purchase;
+DROP INDEX IF EXISTS idx_purchaseproduct;
+DROP INDEX IF EXISTS idx_productreview;
+
+
+
 -- Tables
 
 CREATE TABLE users (
@@ -173,9 +202,221 @@ CREATE TABLE brandBrandManager (
   PRIMARY KEY(idBrand, idBrandManager)
 );
 
+
+
+-- Queries
+
+
+CREATE VIEW "ListProducts" AS
+SELECT *
+FROM product;
+
+CREATE VIEW "ViewProduct" AS
+SELECT *
+FROM product
+WHERE id = 1;
+
+CREATE VIEW "ListProductsByCategory" AS
+SELECT *
+FROM product
+WHERE id_category = (SELECT id FROM productcategory WHERE categoryName = 'Fashion');
+
+CREATE VIEW "ReviewsByProductId" AS
+SELECT *
+FROM productreview
+WHERE id_product= 1;
+
+CREATE VIEW "ReviewsByPurchaseId" AS
+SELECT *
+FROM productreview
+WHERE id_purchase = 1;
+
+CREATE VIEW "ListBrands" AS
+SELECT *
+FROM brand;
+
+CREATE VIEW "BrandProducts" AS
+SELECT *
+FROM product
+WHERE id_brand=(SELECT id FROM brand WHERE name = 'Alameda Turquesa');
+
+CREATE VIEW "ListUsers" AS
+SELECT *
+FROM users;
+
+CREATE VIEW "Profile" AS
+SELECT *
+FROM users
+WHERE id = 1;
+
+CREATE VIEW "Supports" AS
+SELECT *
+FROM users
+LEFT OUTER JOIN chatSupport
+ON users.id = chatSupport.id_chatSupport;
+
+CREATE VIEW "BrandManagers" AS
+SELECT *
+FROM users
+INNER JOIN brandManager
+ON users.id = brandManager.id_brandManager;
+
+CREATE VIEW "BrandManagersByBrand" AS
+SELECT *
+FROM brandManager
+WHERE id_brandManager = (SELECT idBrandManager FROM brandBrandManager WHERE idBrand = (SELECT id FROM brand WHERE name='Alameda Turquesa'));
+
+CREATE VIEW "WishlistByClientId" AS
+SELECT product.name, product.price, product.imageURL, client.id_client
+FROM product, client, productwishlist
+WHERE client.id_client = productwishlist.id_client AND product.id = productwishlist.id_product AND client.id_client = 12;
+
+CREATE VIEW "PurchasesByClientId" AS
+SELECT *
+FROM purchase
+WHERE id_client = 11;
+
+CREATE VIEW "ProductsInPurchase" AS
+SELECT product.name, product.price, product.imageURL, purchase.id_client, purchaseproduct.quantity, purchaseproduct.cost
+FROM purchase, product, purchaseproduct
+WHERE purchase.id = purchaseproduct.id_purchase AND product.id = purchaseproduct.id_product AND purchase.id_client = 12;
+
+CREATE VIEW "ProductsInCart" AS
+SELECT product.name, product.price, product.imageURL, client.id_client
+FROM product, client, cartproduct
+WHERE client.id_client = cartproduct.id_client AND product.id = cartproduct.id_product AND client.id_client = 11;
+
+CREATE VIEW "ClientMessages" AS
+SELECT *
+FROM message
+WHERE id_client = 11;
+
+
+UPDATE users
+SET
+ firstName = $firstName,
+ lastName = $lastName,
+ username = $username,
+ email = $email,
+ password = $password,
+ imageURL = $imageURL,
+ dateModified = now()
+WHERE id = $id;
+
+INSERT INTO users(id, firstName, lastName, username, email, password, imageURL, dateCreated, dateModified, active)
+  VALUES ($id, $firstName, $lastName, $username, $email, $password, $imageURL, dateCreated, dateModified, active);
+
+DELETE FROM admin WHERE id = (SELECT id FROM users WHERE id=$id);
+
+DELETE FROM brandManager WHERE id=$id;
+
+DELETE FROM users WHERE id=$id;
+
+INSERT INTO message (id, message, dateSent, sender, id_chatSupport, id_client)
+  VALUES($id, $message, now(), $sender, $id_chatSupport, $id_client);
+
+DELETE FROM message WHERE id=$id;
+
+DELETE FROM brandManager WHERE id_brandManager = (SELECT id FROM users WHERE id=$id);
+
+UPDATE brand
+SET
+ name=$name,
+ contact=$contact
+WHERE id=$id;
+
+INSERT INTO brand (id, name, contact)
+  VALUES ($id, $name, $contact);
+
+DELETE FROM brandBrandManager WHERE idBrand = (SELECT id FROM brand WHERE name=$name);
+
+DELETE FROM brand WHERE name=$name;
+
+INSERT INTO ban (id_user, id_admin, banDate)
+  VALUES ($id_user, $id_admin, $banDate);
+
+DELETE FROM ban WHERE id_user=$id_user;
+
+UPDATE product
+SET
+ quantityInStock=$quantityInStock,
+ modelNumber=$modelNumber,
+ weight=$weight,
+ price=$price,
+ imageURL=$imageURL,
+ bigDescription=$bigDescription,
+ shortDescription=$shortDescription,
+ id_category=$id_category
+WHERE id_brand=$id_brand
+AND name=$name;
+
+INSERT INTO product(id, name, quantityInStock, modelNumber, weight, price, imageURL, bigDescription, shortDescription, id_brand, id_category)
+  VALUES($id, $name, $quantityInStock, $modelNumber, $weight, $price, $imageURL, $bigDescription, $shortDescription, $id_brand, $id_category);
+
+DELETE FROM product WHERE id_product=$id_product;
+
+UPDATE productcategory
+SET
+ categoryName=$categoryName
+WHERE id=$id;
+
+INSERT INTO productreview(id_product, id_purchase, textReview, rating)
+  VALUES ($id_product, $id_purchase, $textReview, $rating);
+
+DELETE FROM productreview WHERE id_product=$id_product AND id_purchase=$id_purchase;
+
+DELETE FROM cartproduct WHERE id_client = (SELECT id FROM users WHERE id=$id);
+
+DELETE FROM productwishlist WHERE id_client = (SELECT id FROM users WHERE id=$id);
+
+DELETE FROM client WHERE id_client = (SELECT id FROM users WHERE id=$id);
+
+DELETE FROM users WHERE id=$id;
+
+UPDATE client
+SET
+ cellphone = $cellphone
+WHERE id_client=$id_client;
+
+INSERT INTO client(id_client, cellphone)
+  VALUES ($id_client, $cellphone);
+
+INSERT INTO productwishlist (id_product, id_client)
+  VALUES ($id_product, $id_client);
+
+DELETE FROM productwishlist WHERE id_product=$id_product AND id_client=$id_client;
+
+UPDATE cartproduct
+SET
+ quantity=$quantity
+WHERE id_product=$id_product AND id_client=$id_client;
+
+INSERT INTO cartproduct (id_client, id_product, quantity)
+  VALUES ($id_client, $id_product, $quantity);
+
+DELETE FROM cartproduct WHERE id_product=$id_product AND id_client=$id_client;
+
+INSERT INTO city (id, city, id_country)
+  VALUES ($id, $city, $id_country);
+
+INSERT INTO address (id, address, zipcode, id_city)
+  VALUES($id, $address, $zipcode, $id_city);
+
+DELETE FROM clientaddress WHERE id_client=$id_client AND id_address=(SELECT id FROM address WHERE address=$address);
+
+DELETE FROM address WHERE address=$address;
+
+INSERT INTO purchase(id, id_client, id_address, purchaseDate, purchaseState, cost, paymentType, cardNumber, cardName, cardExpirationDate, nif)
+  VALUES ($id_client,$id_address, $purchaseDate, $purchaseState, $cost, $paymentType, $cardNumber, $cardName, $cardExpirationDate, $nif);
+
+INSERT INTO purchaseproduct (id_purchase, id_product, quantity, cost)
+  VALUES ($id_purchase, $id_product, $quantity, $cost);
+
+
+
  -- Indexes
- 
- CREATE INDEX idx_users ON users USING hash (id); /*cardinalidade alta-nao se faz cluster*/
+
+CREATE INDEX idx_users ON users USING hash (id); /*cardinalidade alta-nao se faz cluster*/
 
 CREATE INDEX idx_message ON message USING btree (id_client); /*cardinalidade media-bom candidato para cluster*/
 
@@ -191,8 +432,10 @@ CREATE INDEX idx_purchaseproduct ON purchaseproduct USING hash (id_purchase); /*
 
 CREATE INDEX idx_productreview ON productreview USING hash (id_product); /*cardinalidade media-bom candidato para cluster*/
 
+
+
  -- Triggers
- 
+
  --Trigger that prevents an admin from being banned
 CREATE FUNCTION ban_admin() RETURNS TRIGGER AS
 $BODY$
@@ -223,7 +466,7 @@ BEGIN
 	RETURN NEW;
 END
 $BODY$
-LANGUAGE plpgsql;	
+LANGUAGE plpgsql;
 
 
 
