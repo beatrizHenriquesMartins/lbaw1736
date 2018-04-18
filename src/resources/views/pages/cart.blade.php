00e9a42257fb8c505a6e5453e6e3af095b406910
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-04-18 10:58:05 +0100

    FAQ not complete

--- src/resources/views/pages/cart.blade.php
+++ src/resources/views/pages/cart.blade.php
@@ -2,96 +2,95 @@
 
 
 @section('content')
+    <!-- breadcrumbs -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/homepage">
+                    Homepage
+                </a>
+            </li>
 
-
-
-<!-- breadcrumbs -->
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-            <a href="/homepage">
-                Homepage
-            </a>
-        </li>
-        <li class="breadcrumb-item">
-            <a href="/profile">
-                Profile
-            </a>
-        </li>
-        <li class="breadcrumb-item" aria-current="page">
-            Cart
-        </li>
-    </ol>
-</nav>
-
-<div class="container-fluid main">
-    <div class="row-fluid category-section wishlist-section">
-        <div class = "sidelinks col-sm-2">
-            <div class="list-group">
-                <a href="customer_profile.html" class="list-group-item list-group-item-action">
+            <li class="breadcrumb-item">
+                <a href="/profile">
                     Profile
                 </a>
+            </li>
 
-                <a href="list_purchases.html" class="list-group-item list-group-item-action">
-                    List of Purchases
-                </a>
+            <li class="breadcrumb-item" aria-current="page">
+                Cart
+            </li>
+        </ol>
+    </nav>
 
-                <a href="{{route('wishlist')}}" class="list-group-item list-group-item-action">
-                    List of Favourites
-                </a>
+    <div class="container-fluid main">
+        <div class="row-fluid category-section wishlist-section">
+            <div class = "sidelinks col-sm-2">
+                <div class="list-group">
+                    <a href="customer_profile.html" class="list-group-item list-group-item-action">
+                        Profile
+                    </a>
 
-                <a href="{{route('cart')}}" class="list-group-item list-group-item-action active">
-                    Cart
-                </a>
+                    <a href="list_purchases.html" class="list-group-item list-group-item-action">
+                        List of Purchases
+                    </a>
+
+                    <a href="{{route('wishlist')}}" class="list-group-item list-group-item-action">
+                        List of Favourites
+                    </a>
+
+                    <a href="{{route('cart')}}" class="list-group-item list-group-item-action active">
+                        Cart
+                    </a>
+                </div>
+            </div>
+
+            <div class="category-products wishlist-products col-sm-8 col-sm-offset-1">
+                @each('partials.cartproduct', $carts, 'cart')
             </div>
         </div>
-      <div class="category-products wishlist-products col-sm-8 col-sm-offset-1">
-        @each('partials.cartproduct', $carts, 'cart')
 
-      </div>
+        @if(count($carts)!=0)
+            <div class="final">
+                <div class="delete-cart">
+                    <div class="blankspace">
+                    </div>
+
+                    <div class="delete-cart-btn">
+                        <form class="form-horizontal" method="post" action="{{ route('removeAllCart') }}">
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger">
+                                Delete Cart
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="blankspace">
+                </div>
+
+                <div class="total-order">
+                    <div class="pull-right">
+                        <div class="total">
+                            <h3>
+                                Total
+                            </h3>
+
+                            <h3>
+                                {{$cost}} €
+                            </h3>
+                        </div>
+
+                        <div class="order">
+                            <div class="order-btn">
+                                <a class="btn btn-success pull-right" href="cart_order.html" role="button">
+                                    Order
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
-
-    @if(count($carts)!=0)
-      <div class="final">
-          <div class="delete-cart">
-              <div class="blankspace">
-              </div>
-
-              <div class="delete-cart-btn">
-                <form class="form-horizontal" method="post" action="{{ route('removeAllCart') }}">
-                      {{ csrf_field() }}
-                      <button type="submit" class="btn btn-danger">
-                          Delete Cart
-                      </button>
-                  </form>
-              </div>
-          </div>
-
-          <div class="blankspace">
-          </div>
-
-          <div class="total-order">
-              <div class="pull-right">
-                  <div class="total">
-                      <h3>
-                          Total
-                      </h3>
-
-                      <h3>
-                          {{$cost}} €
-                      </h3>
-                  </div>
-
-                  <div class="order">
-                      <div class="order-btn">
-                          <a class="btn btn-success pull-right" href="cart_order.html" role="button">
-                              Order
-                          </a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    @endif
-</div>
 @endsection
--- src/resources/views/pages/faq.blade.php
+++ src/resources/views/pages/faq.blade.php
@@ -6,49 +6,121 @@
             <h3>
                 FAQ
             </h3>
-        </div>
 
-        <!-- perguntas não colapsadas-->
-        <div id="accordion">
-            <!-- Pergunta 1 -->
-            <div class="card">
-                <div class="card-header" id="headingOne">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapseOne">
-                            Didn't receive an item
-                        </button>
-                    </h5>
+
+            <!-- perguntas não colapsadas-->
+            <div id="accordion">
+                <!-- Pergunta 1 -->
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                    aria-expanded="true" aria-controls="collapseOne">
+                                What is covered by the Amazonas Money Back Guarantee?
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body">
+                            You're covered by the eBay Money Back Guarantee for just about every purchase on eBay.co.uk
+                            when you pay with PayPal. As with every programme like this, there are some rules:
+
+                            <ul>
+                                <li>
+                                    Amazonas Money Back Guarantee protects you in case you do not receive your item or if
+                                    the item is not as described in the listing. If this happens, you need to tell your
+                                    seller within 30 days from your actual or latest estimated delivery date.
+                                </li>
+
+                                <li>
+                                    The programme is designed to protect purchases that are usually sent by post – over
+                                    99% of listings on eBay are. It doesn’t include vehicles, real estate, businesses
+                                    for sale, digital goods or services.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <!-- resposta pergunta -->
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                <div class="card-body">
-                    The Amazonas covers your purchase price plus original shipping for virtually all
-                    items on eBay.com. We guarantee you’ll get the item you ordered or your money back.
+                <!-- Pergunta 2 -->
+                <div class="card">
+                    <div class="card-header" id="headingTwo">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo"
+                                    aria-expanded="true" aria-controls="collapseTwo">
+                                I haven't received an item – what should I do?
+                            </button>
+                        </h5>
+                    </div>
 
-                    If you paid for an item and haven’t received it, there are a few things you can do.
+                    <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
+                        <div class="card-body">
+                            The amount of time that it takes for an item to arrive often depends on the postage method
+                            that you selected when you bought the item. You can review the estimated delivery time on
+                            the Order Details page.
+
+                            If the estimated delivery time has already passed, you should contact the seller. To do so,
+                            go back to contact us and send as an email explain the case.
+                        </div>
+                    </div>
                 </div>
-            </div>
 
+                <!-- Pergunta 3 -->
+                <div id="card">
+                    <div class="card-header" id="headingThree">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree"
+                                    aria-expanded="true" aria-controls="collapseThree">
+                                I received an item that doesn't match the listing description, what should I do?
+                            </button>
+                        </h5>
+                    </div>
 
-            <!-- Pergunta 2 -->
-            <div class="card">
-                <div class="card-header" id="headingOne">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapseOne">
-                            How much time my delivery
-                        </button>
-                    </h5>
+                    <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#accordion">
+                        <div class="card-body">
+                            If you received an item that's wrong, damaged or otherwise not as described, you should
+                            contact us so we can quickly help you with the issue.
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <!-- resposta pergunta -->
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                <div class="card-body">
-                    Saddly we don't have any physics shop right one!
+                <!-- Pergunta 4 -->
+                <div id="card">
+                    <div class="card-header" id="headingFour">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour"
+                                    aria-expanded="true" aria-controls="collapseFour">
+                                I received an item that doesn't match the listing description, what should I do?
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseFour" class="collapse show" aria-labelledby="headingFour" data-parent="#accordion">
+                        <div class="card-body">
+                            If you received an item that's wrong, damaged or otherwise not as described, you should
+                            contact us so we can quickly help you with the issue.
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Pergunta 5 -->
+                <div id="card">
+                    <div class="card-header" id="headingFive">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFive"
+                                    aria-expanded="true" aria-controls="collapseFive">
+                                I received the correct item, but I changed my mind and don't want it any more – what
+                                should I do?
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseFive" class="collapse show" aria-labelledby="headingFive" data-parent="#accordion">
+                        <div class="card-body">
+                            Go to the contact us page and send as an email with the account and we open a process.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
