bc4dc44b71b97a5a8ef80264fcba1719faadca98
Luis Saraiva <up201404302@fe.up.pt>
2018-04-21 18:59:15 +0100

    Add product and edit product

---
+++ src/resources/views/pages/addproduct.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main', ['type' => $type])
+
+@section('content')
+    <!-- breadcrumbs -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/homepage">
+                    Homepage
+                </a>
+            </li>
+
+            <li class="breadcrumb-item active" aria-current="page">
+                Add Product
+            </li>
+        </ol>
+    </nav>
+    <form method="post" action="{{route('addProduct')}}" enctype="multipart/form-data">
+      @include('partials.addproductform')
+    </form>
+@endsection
