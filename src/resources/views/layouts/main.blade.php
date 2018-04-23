bf7aa63c263dc4905e299c0b8c93970bad9924fb
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-04-23 12:43:46 +0100

    pequena mudança no sign in/up

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -86,213 +86,221 @@
     <body>
     <main>
         <!-- cabeçalho -->
-      <div class="first_class">
-          <!-- TOP BAR -->
-          <div class="container-fluid topbar">
-              <div class="row top">
-                  <div class="col-sm-2 flex-item">
-                      <a href="{{ url('/homepage') }}">
-                          <img class="mb-4"  src="/images/logo_1.png" alt="logo" >
-                      </a>
-                  </div>
+        <div class="first_class">
+            <!-- TOP BAR -->
+            <div class="container-fluid topbar">
+                <div class="row top">
+                    <div class="col-sm-2 flex-item">
+                        <a href="{{ url('/homepage') }}">
+                            <img class="mb-4"  src="/images/logo_1.png" alt="logo" >
+                        </a>
+                    </div>
 
-                  <div class="col-sm-3 flex-item search">
-                      <form class="navbar-form navbar-right" role="search" id="navBarSearchForm" action="/action_page.php">
-                          <input class="form-control" type="text" placeholder="Search..." name="search">
+                    <div class="col-sm-3 flex-item search">
+                        <form class="navbar-form navbar-right" role="search" id="navBarSearchForm" action="/action_page.php">
+                            <input class="form-control" type="text" placeholder="Search..." name="search">
 
-                          <a class="btn btn-dark" href="{{ url('/search_result') }}" role="button">
-                              GO!
-                          </a>
-                      </form>
-                  </div>
-
-                  @if (Auth::check())
-                      <div class="col-sm-3 flex-item sign">
-                          <span class="hi">
-                              Hi,
-                          </span>
-
-                          <div class="dropdown">
-                              <button class="btn btn-secondary dropdown-toggle align-text-top" type="button"
-                                      id="dropdownLogin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-                                  {{ Auth::user()->firstname }}
-
-                                  <i class="fa fa-caret-down">
-                                  </i>
-                              </button>
-
-                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                  <li>
-                                      <a class="dropdown-item" href="{{ url('/customer_profile') }}">
-                                          Profile
-                                      </a>
-                                  </li>
-
-                                  <div class="dropdown-divider">
-                                  </div>
-
-                                  <li>
-                                      <a class="dropdown-item" href="{{ url('/logout') }}">
-                                          Sign Out
-                                      </a>
-                                  </li>
-                              </div>
-                          </div>
-                          <div class="user-img">
-                            <a href="{{ url('/profile') }}">
-                                <img class="mb-4"  src="{{Auth::user()->imageurl}}" alt="logo" >
+                            <a class="btn btn-dark" href="{{ url('/search_result') }}" role="button">
+                                GO!
                             </a>
-                          </div>
-                      </div>
-                  @endif
+                        </form>
+                    </div>
 
-                  @if (!Auth::check())
-                      <div class="col-sm-3 flex-item sign">
-                          <a href="{{ url('/login') }}">
-                              Sign in
-                          </a>
+                    @if (Auth::check())
+                        <div class="col-sm-3 flex-item sign">
+                            <span class="hi">
+                                Hi,
+                            </span>
 
-                          <span>
-                              or
-                          </span>
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle align-text-top" type="button"
+                                        id="dropdownLogin" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="false">
+                                    {{ Auth::user()->firstname }}
 
-                          <a href="{{ url('/register') }}">
-                              Sign up
-                          </a>
-                      </div>
-                  @endif
-              </div>
-          </div>
+                                    <i class="fa fa-caret-down">
+                                    </i>
+                                </button>
 
-          <!-- Navigation -->
-          <nav class="navbar navbar-inverse navbar-static-top custom-navbar" role="navigation">
-              <div class="navBar-container">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
-                      <span class="sr-only">
-                          Toggle navigation
-                      </span>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <a class="dropdown-item" href="{{ url('/customer_profile') }}">
+                                            Profile
+                                        </a>
+                                    </li>
 
-                      <span class="icon-bar">
-                      </span>
+                                    <div class="dropdown-divider">
+                                    </div>
 
-                      <span class="icon-bar">
-                      </span>
+                                    <li>
+                                        <a class="dropdown-item" href="{{ url('/logout') }}">
+                                            Sign Out
+                                        </a>
+                                    </li>
+                                </div>
+                            </div>
 
-                      <span class="icon-bar">
-                      </span>
-                  </button>
+                            <div class="user-img">
+                                <a href="{{ url('/profile') }}">
+                                    <img class="mb-4"  src="{{Auth::user()->imageurl}}" alt="logo" >
+                                </a>
+                            </div>
+                        </div>
+                    @endif
 
-                  <!-- Non-collapsing right-side icons -->
-                  <ul class="nav navbar-nav navbar-right">
-                      @if(Auth::check())
-                          <li>
-                              <a href="{{ url('/profile') }}" class="fa fa-user">
-                              </a>
-                          </li>
-                        @if($type == 1)
-                          <li>
-                              <a href="{{ url('/wishlist') }}" class="fa fa-heart">
-                              </a>
-                          </li>
+                    @if (!Auth::check())
+                        <div class="col-sm-3 flex-item sign">
+                            <a href="{{ url('/login') }}">
+                                Sign in
+                            </a>
 
-                          <li>
-                              <a href="{{ url('/cart') }}" class="fa fa-shopping-cart">
-                              </a>
-                          </li>
+                            <span>
+                                or
+                            </span>
+
+                            <a href="{{ url('/register') }}">
+                                Sign up
+                            </a>
+                        </div>
+                    @endif
+                </div>
+            </div>
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-static-top custom-navbar" role="navigation">
+                <div class="navBar-container">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                        <span class="sr-only">
+                            Toggle navigation
+                        </span>
+
+                        <span class="icon-bar">
+                        </span>
+
+                        <span class="icon-bar">
+                        </span>
+
+                        <span class="icon-bar">
+                        </span>
+                    </button>
+
+                    <!-- Non-collapsing right-side icons -->
+                    <ul class="nav navbar-nav navbar-right">
+                        @if(Auth::check())
+                            <li>
+                                <a href="{{ url('/profile') }}" class="fa fa-user">
+                                </a>
+                            </li>
+
+                            @if($type == 1)
+                                <li>
+                                    <a href="{{ url('/wishlist') }}" class="fa fa-heart">
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="{{ url('/cart') }}" class="fa fa-shopping-cart">
+                                    </a>
+                                </li>
+                            @endif
+
+                            @if($type == 2)
+                                <li>
+                                    <a href="{{ url('/newproduct') }}" class="fa fa-plus">
+                                    </a>
+                                </li>
+                            @endif
+
+                            @if($type == 3)
+                                <li>
+                                    <a href="{{ url('/messages') }}" class="fa fa-plus">
+                                    </a>
+                                </li>
+                            @endif
+
+                            @if($type == 4)
+                                <li>
+                                    <a href="{{ url('/clients') }}" class="fa fa-users">
+                                    </a>
+                                </li>
+                            @endif
                         @endif
-                        @if($type == 2)
-                          <li>
-                              <a href="{{ url('/newproduct') }}" class="fa fa-plus">
-                              </a>
-                          </li>
+
+                        @if(!Auth::check())
+                            <li>
+                                <a href="{{ url('/login') }}" class="fa fa-sign-in">
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="{{ url('/register') }}" class="fa fa-user-plus">
+                                </a>
+                            </li>
                         @endif
-                        @if($type == 3)
-                          <li>
-                              <a href="{{ url('/messages') }}" class="fa fa-plus">
-                              </a>
-                          </li>
-                        @endif
-                        @if($type == 4)
-                          <li>
-                              <a href="{{ url('/clients') }}" class="fa fa-users">
-                              </a>
-                          </li>
-                        @endif
-                      @endif
-                      @if(!Auth::check())
+
                         <li>
-                            <a href="{{ url('/login') }}" class="fa fa-sign-in">
+                            <a href="{{ url('/homepage') }}" class="fa fa-home">
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ url('/register') }}" class="fa fa-user-plus">
-                            </a>
-                        </li>
-                      @endif
-                      <li>
-                          <a href="{{ url('/homepage') }}" class="fa fa-home">
-                          </a>
-                      </li>
-                  </ul>
+                    </ul>
 
-                  <!-- the collapsing menu -->
-                  <div class="collapse navbar-collapse navbar-left" id="navbar-collapse-1">
-                      <ul class="nav navbar-nav">
-                          @each('partials.category', $categories, 'category')
-                      </ul>
-                  </div>
-              </div>
-          </nav>
-      </div>
+                    <!-- the collapsing menu -->
+                    <div class="collapse navbar-collapse navbar-left" id="navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            @each('partials.category', $categories, 'category')
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
 
-      <section id="content">
-          @yield('content')
-      </section>
+        <section id="content">
+            @yield('content')
+        </section>
 
-      <!--Company Links-->
-      <div>
-          <div class ="company_links container-fluid">
-              <div class="col-sm-10">
-              </div>
+        <!--Company Links-->
+        <div>
+            <div class ="company_links container-fluid">
+                <div class="col-sm-10">
+                </div>
 
-              <div class="col-sm-2">
-                  <div class="row-fluid">
-                      <a href="{{ url('/aboutus') }}">
-                          <h4>
-                              About Amazonas
-                          </h4>
-                      </a>
-                  </div>
+                <div class="col-sm-2">
+                    <div class="row-fluid">
+                        <a href="{{ url('/aboutus') }}">
+                            <h4>
+                                About Amazonas
+                            </h4>
+                        </a>
+                    </div>
 
-                  <div class="row-fluid">
-                      <a href="{{ url('/faq') }}">
-                          FAQ
-                      </a>
-                  </div>
+                    <div class="row-fluid">
+                        <a href="{{ url('/faq') }}">
+                            FAQ
+                        </a>
+                    </div>
 
-                  <div class="row-fluid">
-                      <a href="{{ url('/contactus') }}">
-                          Contact us
-                      </a>
-                  </div>
+                    <div class="row-fluid">
+                        <a href="{{ url('/contactus') }}">
+                            Contact us
+                        </a>
+                    </div>
 
-                  <div class="row-fluid">
-                      <a href="{{ url('/terms') }}">
-                          Terms & Conditions
-                      </a>
-                  </div>
-              </div>
-          </div>
+                    <div class="row-fluid">
+                        <a href="{{ url('/terms') }}">
+                            Terms & Conditions
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-          <div>
-              <footer id="pageFooter">
-                  <p>
-                      Powered by LBAW1736 - MIEIC 2017/2018
-                  </p>
-              </footer>
-          </div>
-      </div>
-  </main>
-  </body>
+            <div>
+                <footer id="pageFooter">
+                    <p>
+                        Powered by LBAW1736 - MIEIC 2017/2018
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </main>
+    </body>
 </html>
