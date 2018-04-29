f003ef230b60facc3ecdee91c967b38997715137
francis-andrade <up201503481@fe.up.pt>
2018-04-29 01:48:12 +0100

    transaction added

--- src/sql/transactions.sql
+++ src/sql/transactions.sql
@@ -41,3 +41,27 @@ INSERT INTO purchaseproducts (id_purchase, id_product, quantity, cost)
 VALUES ($id, $id_product, $quantity, $cost);
 
 COMMIT;
+
+
+
+BEGIN TRANSACTION;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE READ ONLY;
+
+--Get number of purchases
+SELECT COUNT(*)
+FROM products ;
+
+--Get products 
+SELECT p.id AS "ID",p.name AS "Name", p.quantityInStock AS "Quantity In Stock", 
+	   p.dateCreated AS "Date Created",  
+        p.price AS "Price", 
+	   p.imageURL AS "Image URL", p.bigDescription AS "Big Description", 
+       p.shortDescription AS "Short Description", 
+	   (array_agg(pc.categoryName ORDER BY p.id DESC))[1] AS "Category", 
+       (array_agg(b.brandname ORDER BY p.id DESC))[1] AS "Brand", 
+       AVG(pr.rating) AS "Rating"
+FROM products p, categories pc, brands b, reviews pr
+WHERE p.id_brand = b.id_brand AND p.id_category = pc.id_category AND pr.id_product = p.id 
+GROUP BY p.id;
+
+COMMIT;
