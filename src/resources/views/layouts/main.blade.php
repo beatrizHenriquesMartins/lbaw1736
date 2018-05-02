5d225ab552f999be9fc5e483c3ffa1387d11144b
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-05-02 12:12:14 +0100

    correcção pequeno erro no ficheiro main

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -93,209 +93,232 @@
 
     <body>
     <main>
-      <div class="teste">
-          <!-- cabeçalho -->
-        <div class="first_class">
-            <!-- TOP BAR -->
-            <div class="container-fluid topbar">
-                <div class="row top">
-                    <div class="col-sm-2 flex-item">
-                        <a href="{{ url('/homepage') }}">
-                            <img class="mb-4"  src="/images/logo_1.png" alt="logo" >
-                        </a>
-                    </div>
+        <div class="teste">
+            <!-- cabeçalho -->
+            <div class="first_class">
+                <!-- TOP BAR -->
+                <div class="container-fluid topbar">
+                    <div class="row top">
+                        <div class="col-sm-2 flex-item">
+                            <a href="{{ url('/homepage') }}">
+                                <img class="mb-4"  src="/images/logo_1.png" alt="logo" >
+                            </a>
+                        </div>
 
-                    <div class="col-sm-3 flex-item search">
-                        <form class="navbar-form navbar-right" id="navBarSearchForm" action="{{route('search')}}" method="get">
-                            <input class="form-control" type="text" placeholder="Search..." name="input">
+                        <div class="col-sm-3 flex-item search">
+                            <form class="navbar-form navbar-right" id="navBarSearchForm" action="{{route('search')}}"
+                                  method="get">
 
-                            <button type="submit" class="btn btn-dark">
-                                GO!
-                            </button>
-                        </form>
-                    </div>
+                                <input class="form-control" type="text" placeholder="Search..." name="input">
 
-                    @if (Auth::check())
-                        <div class="col-sm-3 flex-item sign">
-                            <span class="hi">
-                                Hi,
-                            </span>
-
-                            <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle align-text-top" type="button"
-                                        id="dropdownLogin" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                    {{ Auth::user()->firstname }}
-
-                                    <i class="fa fa-caret-down">
-                                    </i>
+                                <button type="submit" class="btn btn-dark">
+                                    GO!
                                 </button>
+                            </form>
+                        </div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                    <li>
-                                        <a class="dropdown-item" href="{{ url('/profile') }}">
-                                            Profile
-                                        </a>
-                                    </li>
+                        @if (Auth::check())
+                            <div class="col-sm-3 flex-item sign">
+                                <span class="hi">
+                                    Hi,
+                                </span>
 
-                                    <div class="dropdown-divider">
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle align-text-top" type="button"
+                                            id="dropdownLogin" data-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">
+
+                                        {{ Auth::user()->firstname }}
+
+                                        <i class="fa fa-caret-down">
+                                        </i>
+                                    </button>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                        <li>
+                                            <a class="dropdown-item" href="{{ url('/profile') }}">
+                                                Profile
+                                            </a>
+                                        </li>
+
+                                        <div class="dropdown-divider">
+                                        </div>
+
+                                        <li>
+                                            <a class="dropdown-item" href="{{ url('/logout') }}">
+                                                Sign Out
+                                            </a>
+                                        </li>
                                     </div>
+                                </div>
 
-                                    <li>
-                                        <a class="dropdown-item" href="{{ url('/logout') }}">
-                                            Sign Out
-                                        </a>
-                                    </li>
+                                <div class="user-img">
+                                    <a href="{{ url('/profile') }}">
+                                        <img class="mb-4"  src="{{Auth::user()->imageurl}}" alt="logo" >
+                                    </a>
+
+                                    <!-- </form> -->
                                 </div>
                             </div>
-                            <div class="user-img">
-                              <a href="{{ url('/profile') }}">
-                                  <img class="mb-4"  src="{{Auth::user()->imageurl}}" alt="logo" >
-                              </a>
-                          </form>
-                      </div>
-                      @endif
+                        @endif
 
-                      @if (!Auth::check())
-                          <div class="col-sm-3 flex-item sign">
-                              <a href="{{ url('/login') }}">
-                                  Sign in
-                              </a>
+                        @if (!Auth::check())
+                            <div class="col-sm-3 flex-item sign">
+                                <a href="{{ url('/login') }}">
+                                    Sign in
+                                </a>
 
-                              <span>
-                                  or
-                              </span>
+                                <span>
+                                    or
+                                </span>
 
-                              <a href="{{ url('/register') }}">
-                                  Sign up
-                              </a>
-                          </div>
-                      @endif
-                  </div>
-              </div>
+                                <a href="{{ url('/register') }}">
+                                    Sign up
+                                </a>
+                            </div>
+                        @endif
+                            <!-- </div> -->
+                    </div>
+                </div>
 
-              <!-- Navigation -->
-              <nav class="navbar navbar-inverse navbar-static-top custom-navbar" role="navigation">
-                  <div class="navBar-container">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
-                          <span class="sr-only">
-                              Toggle navigation
-                          </span>
+                <!-- Navigation -->
+                <nav class="navbar navbar-inverse navbar-static-top custom-navbar" role="navigation">
+                    <div class="navBar-container">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                            <span class="sr-only">
+                                Toggle navigation
+                            </span>
 
-                          <span class="icon-bar">
-                          </span>
+                            <span class="icon-bar">
+                            </span>
 
-                          <span class="icon-bar">
-                          </span>
+                            <span class="icon-bar">
+                            </span>
 
-                          <span class="icon-bar">
-                          </span>
-                      </button>
+                            <span class="icon-bar">
+                            </span>
+                        </button>
 
-                      <!-- Non-collapsing right-side icons -->
-                      <ul class="nav navbar-nav navbar-right">
-                          @if(Auth::check())
-                              <li>
-                                  <a href="{{ url('/profile') }}" class="fa fa-user">
-                                  </a>
-                              </li>
+                        <!-- Non-collapsing right-side icons -->
+                        <ul class="nav navbar-nav navbar-right">
+                            @if(Auth::check())
+                                <li>
+                                    <a href="{{ url('/profile') }}" class="fa fa-user">
+                                    </a>
+                                </li>
 
-                              @if($type == 1)
-                                  <li>
-                                      <a href="{{ url('/wishlist') }}" class="fa fa-heart">
-                                      </a>
-                                  </li>
+                                @if($type == 1)
+                                    <li>
+                                        <a href="{{ url('/wishlist') }}" class="fa fa-heart">
+                                        </a>
+                                    </li>
 
-                                  <li>
-                                      <a href="{{ url('/cart') }}" class="fa fa-shopping-cart">
-                                      </a>
-                                  </li>
-                              @endif
+                                    <li>
+                                        <a href="{{ url('/cart') }}" class="fa fa-shopping-cart">
+                                        </a>
+                                    </li>
+                                @endif
 
-                              @if($type == 2)
-                                  <li>
-                                      <a href="{{ url('/newproduct') }}" class="fa fa-plus">
-                                      </a>
-                                  </li>
-                              @endif
+                                @if($type == 2)
+                                    <li>
+                                        <a href="{{ url('/newproduct') }}" class="fa fa-plus">
+                                        </a>
+                                    </li>
+                                @endif
 
-                              @if($type == 3)
-                                  <li>
-                                      <a href="{{ url('/messages') }}" class="fa fa-plus">
-                                      </a>
-                                  </li>
-                              @endif
+                                @if($type == 3)
+                                    <li>
+                                        <a href="{{ url('/messages') }}" class="fa fa-plus">
+                                        </a>
+                                    </li>
+                                @endif
 
-                              @if($type == 4)
-                                  <li>
-                                      <a href="{{ url('/clients') }}" class="fa fa-users">
-                                      </a>
-                                  </li>
-                              @endif
-                          @endif
+                                @if($type == 4)
+                                    <li>
+                                        <a href="{{ url('/clients') }}" class="fa fa-users">
+                                        </a>
+                                    </li>
+                                @endif
+                            @endif
 
-                          @if(!Auth::check())
-                              <li>
-                                  <a href="{{ url('/login') }}" class="fa fa-sign-in">
-                                  </a>
-                              </li>
+                            @if(!Auth::check())
+                                <li>
+                                    <a href="{{ url('/login') }}" class="fa fa-sign-in">
+                                    </a>
+                                </li>
 
-                              <li>
-                                  <a href="{{ url('/register') }}" class="fa fa-user-plus">
-                                  </a>
-                              </li>
-                          @endif
+                                <li>
+                                    <a href="{{ url('/register') }}" class="fa fa-user-plus">
+                                    </a>
+                                </li>
+                            @endif
 
-                          <li>
-                              <a href="{{ url('/homepage') }}" class="fa fa-home">
-                              </a>
-                          </li>
-                      </ul>
+                            <li>
+                                <a href="{{ url('/homepage') }}" class="fa fa-home">
+                                </a>
+                            </li>
+                        </ul>
 
-                      <!-- the collapsing menu -->
-                      <div class="collapse navbar-collapse navbar-left" id="navbar-collapse-1">
-                          <ul class="nav navbar-nav">
-                              @each('partials.category', $categories, 'category')
-                          </ul>
-                      </div>
-                  </div>
-              </nav>
-          </div>
+                        <!-- the collapsing menu -->
+                        <div class="collapse navbar-collapse navbar-left" id="navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                @each('partials.category', $categories, 'category')
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
 
-          <section id="content">
-              @yield('content')
-          </section>
+            <section id="content">
+                @yield('content')
+            </section>
 
-          @if($type == 1)
-          <div class="container" id="container-chat">
-              <div class="row chat-window col-xs-12 col-md-3" id="chat_window_1" style="margin-left:10px;">
-                  <div class="col-xs-12 col-md-12">
-                  	<div class="panel panel-default">
-                          <div class="panel-heading top-bar">
-                              <div class="col-md-8 col-xs-8">
-                                  <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat </h3>
-                              </div>
-                              <div class="col-md-4 col-xs-4" style="text-align: right;">
-                                  <a href="#"><span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim"></span></a>
-                              </div>
-                          </div>
-                          <div class="panel-body msg_container_base">
-                              @each('partials.messages', $messages, 'message')
-                          </div>
-                          <div class="panel-footer">
-                              <div class="input-group">
-                                  <input id="btn-input" type="text" class="form-control input-sm chat_input" placeholder="Write your message here..." />
-                                  <span class="input-group-btn">
-                                  <a class="btn btn-primary btn-sm" id="btn-chat">Send</a>
-                                  </span>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          @endif
+            @if($type == 1)
+                <div class="container" id="container-chat">
+                    <div class="row chat-window col-xs-12 col-md-3" id="chat_window_1" style="margin-left:10px;">
+                        <div class="col-xs-12 col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading top-bar">
+                                    <div class="col-md-8 col-xs-8">
+                                        <h3 class="panel-title">
+                                            <span class="glyphicon glyphicon-comment">
+                                            </span>
+
+                                            Chat
+                                        </h3>
+                                    </div>
+
+                                    <div class="col-md-4 col-xs-4" style="text-align: right;">
+                                        <a href="#">
+                                            <span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim">
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div class="panel-body msg_container_base">
+                                    @each('partials.messages', $messages, 'message')
+                                </div>
+
+                                <div class="panel-footer">
+                                    <div class="input-group">
+                                        <input id="btn-input" type="text" class="form-control input-sm chat_input"
+                                               placeholder="Write your message here..." />
+
+                                        <span class="input-group-btn">
+                                            <a class="btn btn-primary btn-sm" id="btn-chat">
+                                                Send
+                                            </a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
+
         <!--Company Links-->
         <div>
             <div class ="company_links container-fluid">
