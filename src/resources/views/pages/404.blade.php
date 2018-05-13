48444d30af008850152abaa061b3f619d0eedec7
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-05-13 19:30:44 +0100

    page 404 ✅

--- src/resources/views/pages/404.blade.php
+++ src/resources/views/pages/404.blade.php
@@ -3,9 +3,15 @@
 
 @section('content')
     <div id="content_page_404" class="container d-sm-flex">
-        <div id="all_content">
-            <div id="photo_404">
-                <img src="./images/amazonia.jpg" id="photo_img_404" class="img-responsive" alt="amazonia">
+        <div id="div_content_404" class="card-columns col-sm-2">
+            <div id="image_404">
+                <img id="image_error_404" src="./images/error_404.png">
+            </div>
+
+            <div id="message_404">
+                <h3>
+                    Sorry but this page don't exist!
+                </h3>
             </div>
         </div>
     </div>
