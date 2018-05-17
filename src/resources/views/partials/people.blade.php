0ddb689872c39ab967aa39c7944f395d02a39d8b
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-05-17 12:21:15 +0100

    correcção de alguns erros

--- src/resources/views/partials/people.blade.php
+++ src/resources/views/partials/people.blade.php
@@ -1,7 +1,19 @@
-<div id="people" class="card-columns col-sm-1">
-    <img id="photo_user" src="{{$people->imageurl}}">
+<div id="people" class="card-columns col-sm-3">
+    <div class="card-columns col-sm-1">
+        <img id="photo_user" src="{{$people->imageurl}}">
+    </div>
 
-    <h5>
-        {{$people->firstname}} {{$people->lastname}}
-    </h5>
+    <div id="nameClient" class="card-columns col-sm-1">
+        <h5>
+            {{$people->firstname}} {{$people->lastname}}
+        </h5>
+    </div>
+
+    <div id="hours" class="card-columns col-sm-1">
+        <!--
+        <abbr class="_lht7 timstamp" title="Hoje" data-utime="1526510852.556">
+            12:21
+        </abbr>
+        -->
+    </div>
 </div>
