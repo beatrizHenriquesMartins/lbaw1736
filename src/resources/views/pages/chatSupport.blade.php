d6a1b0136a48dc7b444520b8c08ac60d8b624f80
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-05-17 01:39:30 +0100

    lado das pessoas okay!
    falta colocar scroll

--- src/resources/views/pages/chatSupport.blade.php
+++ src/resources/views/pages/chatSupport.blade.php
@@ -5,6 +5,24 @@
     <div class="container">
         <div class="card-columns col-sm-12">
             <div id="listOfPeople" class="card-columns col-sm-3">
+                <div id="optionsChat" class="card-columns col-sm-3">
+                    <div id="definition" class="card-columns col-sm-1">
+                        <a id="definitionIcon" href="" class="fa fa-cog">
+                        </a>
+                    </div>
+
+                    <div id="sptTitle" class="card-columns col-sm-1">
+                        <h5 id="chatTitle">
+                            Chat Support
+                        </h5>
+                    </div>
+
+                    <div id="newMessage" class="card-columns col-sm-1">
+                        <a id="newMessageIcon" href="" class="fa fa-edit">
+                        </a>
+                    </div>
+                </div>
+
                 <div id="searchChatByPeople" class="card-columns col-sm-3">
                     <input id="searchUserChat" type="text" name="search" placeholder="Search..">
                 </div>
@@ -13,12 +31,10 @@
 
             </div>
 
-            <div class="card-columns col-sm-9">
-                <div id="message" class="card-columns col-sm-1">
-                    <h5>
-                        message
-                    </h5>
-                </div>
+            <div id="message" class="card-columns col-sm-9">
+                <h5>
+                    message
+                </h5>
             </div>
         </div>
     </div>
